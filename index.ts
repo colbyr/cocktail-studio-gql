4a541a9fd8097f9ae0a71faf27c89fbbf934aefa
Colby Rabideau <me@colbyr.com>
2023-07-04 10:50:49 -0400

    nexus and apollo

--- index.ts
+++ index.ts
@@ -1,1 +1,38 @@
-console.log("Hello via Bun!");
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { makeSchema, queryType } from 'nexus';
+import { join } from "path";
+
+const QueryType = queryType({
+  definition(t) {
+      t.string("hello", {
+        resolve: () => "world"
+      })
+  },
+})
+
+const schema = makeSchema({
+  types: [
+    QueryType
+  ],
+  outputs: {
+    typegen: join(__dirname, '../nexus-typegen.ts'),
+    schema: join(__dirname, '../schema.graphql')
+  }
+})
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  schema,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
