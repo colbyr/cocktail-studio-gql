b42b166aab101114edbe62833b85ce45bfee927b
Colby Rabideau <me@colbyr.com>
2023-07-13 23:19:01 -0400

    fix no ingredients

--- src/Recipe/CreateRecipeMutation.ts
+++ src/Recipe/CreateRecipeMutation.ts
@@ -12,39 +12,6 @@ export const CreateRecipeMutation = mutationField('createRecipe', {
   },
   resolve: async (_, { name, recipeIngredients }, { sql, userId }) => {
     return sql.begin(async (sql) => {
-      await sql`
-        INSERT INTO ingredient
-        ${sql(
-          recipeIngredients.map(({ ingredientName }) => ({
-            name: ingredientName,
-            user_id: userId,
-          })),
-        )}
-        ON CONFLICT DO NOTHING
-      `;
-      const idx_ingredient_map = indexBy(
-        ({ idx }) => idx,
-        await sql`
-          WITH new_ingredient AS (
-            SELECT
-              idx,
-              tsvector_name(name) as name_vector
-            FROM (VALUES ${sql(
-              recipeIngredients.map(({ ingredientName }, idx) => [
-                idx,
-                ingredientName,
-              ]),
-            )}) as t(idx, name)
-          )
-
-          SELECT new_ingredient.idx, ingredient.id
-          FROM new_ingredient
-          JOIN ingredient ON (
-            ingredient.user_id = ${userId}
-            AND ingredient.name_vector = new_ingredient.name_vector
-          )
-        `,
-      );
       const [recipe] = z.array(ZRecipe).parse(
         await sql`
           INSERT INTO recipe
@@ -52,18 +19,53 @@ export const CreateRecipeMutation = mutationField('createRecipe', {
           RETURNING *
         `,
       );
-      await sql`
-        INSERT INTO recipe_ingredient
-        ${sql(
-          recipeIngredients.map(({ amount, amountScale }, idx) => ({
-            ingredient_id: idx_ingredient_map[idx].id,
-            recipe_id: recipe.id,
-            user_id: userId,
-            amount,
-            amount_scale: amountScale ?? 1,
-          })),
-        )}
-      `;
+      if (recipeIngredients.length > 0) {
+        await sql`
+          INSERT INTO ingredient
+          ${sql(
+            recipeIngredients.map(({ ingredientName }) => ({
+              name: ingredientName,
+              user_id: userId,
+            })),
+          )}
+          ON CONFLICT DO NOTHING
+        `;
+        const idx_ingredient_map = indexBy(
+          ({ idx }) => idx,
+          await sql`
+            WITH new_ingredient AS (
+              SELECT
+                idx,
+                tsvector_name(name) as name_vector
+              FROM (VALUES ${sql(
+                recipeIngredients.map(({ ingredientName }, idx) => [
+                  idx,
+                  ingredientName,
+                ]),
+              )}) as t(idx, name)
+            )
+
+            SELECT new_ingredient.idx, ingredient.id
+            FROM new_ingredient
+            JOIN ingredient ON (
+              ingredient.user_id = ${userId}
+              AND ingredient.name_vector = new_ingredient.name_vector
+            )
+          `,
+        );
+        await sql`
+          INSERT INTO recipe_ingredient
+          ${sql(
+            recipeIngredients.map(({ amount, amountScale }, idx) => ({
+              ingredient_id: idx_ingredient_map[idx].id,
+              recipe_id: recipe.id,
+              user_id: userId,
+              amount,
+              amount_scale: amountScale ?? 1,
+            })),
+          )}
+        `;
+      }
       return recipe;
     });
   },
